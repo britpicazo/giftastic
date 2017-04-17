d7c95c621ecab4d8608543ced7707788b69439ee
Brittany Picazo <britpicazo@gmail.com>
2017-04-16 23:28:18 -0500

    Working still gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,10 +11,38 @@ $(document).ready(function(){
 	// Adding a button for movie entered
 	$("#add-movie").on("click", function() {
 		event.preventDefault();
-		var movie = $("#movie-input").val();
-		$("#movie-buttons").append("<button class='btn btn-success' data-movie='" + movies[i] + "'>" + movies[i] + "</button>");        
+		var movie = $("#movie-input").val().trim();
+		console.log(movie);
+		$("#movie-buttons").append("<button class='btn btn-success' data-movie='" + movie + "'>" + movie + "</button>");        
 	});
 
-
 	renderButtons();
+
+	// Getting gifs from api... onto html
+	$("button").on("click", function() {
+		var movie = $(this).attr("data-movie");
+		console.log(movie);
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	        movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+	    console.log(queryURL);
+
+	    $.ajax({
+	    	url: queryURL,
+	    	method: "GET"
+	    }).done(function(response){
+	    	var results = response.data;
+	    	console.log(results.length);
+	    	for(var i = 0; i < results.length; i++){
+		    	var movieDiv = $("<div>");
+	          	var p = $("<p>").text("Rating: " + results[i].rating);
+	          	var movieImg = $("<img>");
+
+	          	movieImg.attr("src", results[i].images.fixed_height_still.url);
+	          	movieDiv.append(p);
+	          	movieDiv.append(movieImg);
+	          	$("#movies").append(movieDiv);
+	         }
+	    });
+	});	
+
 });
